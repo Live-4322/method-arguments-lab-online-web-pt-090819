9ed8cbb600db673a2bea1489634bebf4969da6fb
Charlie Oster <oster.charlie@yahoo.com>
2019-09-11 22:45:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,13 @@
-def introduction(name,language)
+def introduction(name)
+   puts "Hi my name is #{name}."
+end
+
+introduction("Dan")
+
+
+
+def introduction_with_language(name,language)
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
 
-introduction("Dan","Ember.js\n")
+introduction_with_language("Dan","Ember.js\n")
